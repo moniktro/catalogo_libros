7d2ab4bf5396bd28eb57278e1df068171091f5db
moniktro <moniktro@gmail.com>
2013-02-10 20:58:05 -0500

    primer commit monica

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatalogoLibros::Application.config.secret_token = '22f3f28d134df6fd9043b47e4cd7bdbabb3c91b590e654ba76350a70c7815af0e3966c82bfac868934f57ca21a31ccb2abf49de908737e62749edf6806a24d7c'
